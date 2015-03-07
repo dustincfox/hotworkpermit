3df4f2f9f8ec742cd0cbfefb48eaefb0fd7731ba
Dustin <dfox@hardhathub.com>
2015-03-07 12:28:34 -0600

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hotworkpermit::Application.config.secret_key_base = 'c574d9a90d64b315d739f938971cf8059dad8a1ce72701363fea4d03c0201ca58a78028455eb69b1fb09f43bcfb6368d27864e7933e2c303b33c464064c6b28f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hotworkpermit::Application.config.session_store :cookie_store, key: '_hotworkpermit_session'
