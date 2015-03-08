74343198316cf32cbc97047079c65c55f67709b2
Dustin <dfox@hardhathub.com>
2015-03-07 21:15:32 -0600

    remove comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Hotworkpermit::Application.routes.draw do
   devise_for :admins
   resources :permits
 
-  # resources :admins
+  resources :admins
 
   resources :projects
 
@@ -10,7 +10,7 @@ Hotworkpermit::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
