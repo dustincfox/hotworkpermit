ef1f6868cf35b08137cf87ef05e3aa98c40b0f21
Adriana C <adriana.lcs316@gmail.com>
2015-03-07 19:21:24 -0600

    add permit associations and migration, add enumerize

--- app/models/permit.rb
+++ app/models/permit.rb
@@ -1,4 +1,8 @@
 class Permit < ActiveRecord::Base
   belongs_to :project;
   belongs_to :admin;
+
+  extend Enumerize
+
+  enumerize :sec1_q6, :sec1_q8, :sec1_q9, :sec1_q10, :sec1_q12, :sec1_q13, in: [:yes, :no, :na]
 end
--- db/migrate/20150307183315_create_permits.rb
+++ db/migrate/20150307183315_create_permits.rb
@@ -1,6 +1,25 @@
 class CreatePermits < ActiveRecord::Migration
   def change
     create_table :permits do |t|
+      t.boolean :sec1_q1
+      t.boolean :sec1_q2
+      t.boolean :sec1_q3
+      t.boolean :sec1_q4
+      t.boolean :sec1_q5
+      t.string :sec1_q6
+      t.boolean :sec1_q7
+      t.string :sec1_q8
+      t.string :sec1_q9
+      t.string :sec1_q10
+      t.boolean :sec1_q11
+      t.string :sec1_q12
+      t.string :sec1_q13
+      t.boolean :sec1_q14
+      t.string :firewatch_name
+
+      t.datetime :date
+
+
 
       t.timestamps
     end
